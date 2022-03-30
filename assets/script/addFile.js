be4c508432c049d3315ebea1b75d3db4b3386eec
Jameel Ahmed <13jameelahmed@gmail.com>
2022-03-30 16:10:26 +0500

    Updated All Forms

--- assets/script/addFile.js
+++ assets/script/addFile.js
@@ -114,3 +114,256 @@ $(document).ready(function() {
         x--;
     })
 });
+
+
+// dining hall field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".diningHall-input-field"); //Fields wrapper
+    var add_button = $(".add-diningHall-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+// kitchen  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".kitchen-input-field"); //Fields wrapper
+    var add_button = $(".add-kitchen-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+
+// hotel Furniture and fixture forms addition
+
+// bedroom  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".bedRoom-input-field"); //Fields wrapper
+    var add_button = $(".add-bedRoom-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+// common room  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".commonRoom-input-field"); //Fields wrapper
+    var add_button = $(".add-commonRoom-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+// corridors and Galleries  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".corridorsGallery-input-field"); //Fields wrapper
+    var add_button = $(".add-corridorsGallery-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+// bathrooms  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".bathrooms-input-field"); //Fields wrapper
+    var add_button = $(".add-bathrooms-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+// commonbathrooms  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".commonbathrooms-input-field"); //Fields wrapper
+    var add_button = $(".add-commonbathrooms-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+// commontoilets  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".commontoilets-input-field"); //Fields wrapper
+    var add_button = $(".add-commontoilets-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-lg-12 my-2"> <label for="" class="form-label">Furniture Type:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+// cuisineServed  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".cuisineServed-input-field"); //Fields wrapper
+    var add_button = $(".add-cuisineServed-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="col-lg-4 col-md-6 my-2"> <label for="" class="form-label"> Cuisine Name:</label> <input type="text" class="form-control" /> <a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+
+
+// common bathrooms number  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".bathroomNo-input-field"); //Fields wrapper
+    var add_button = $(".add-bathroomNo-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-md-6 my-2"> <label for="" class="form-label">Common Bathroom No. :</label> <input type="text" class="form-control" /> </div> <div class="col-md-6 my-2"> <label for="" class="form-label"> Floor No.:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+
+// common toilet number  field form addition
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".toiletNo-input-field"); //Fields wrapper
+    var add_button = $(".add-toiletNo-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-md-6 my-2"> <label for="" class="form-label"> Common Toilet No. :</label> <input type="text" class="form-control" /> </div> <div class="col-md-6 my-2"> <label for="" class="form-label"> Floor No.:</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
