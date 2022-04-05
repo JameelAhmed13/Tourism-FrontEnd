eef253675d3dc96422f13d4a119963c02ccedacd
Jameel Ahmed <13jameelahmed@gmail.com>
2022-04-05 15:31:30 +0500

    tourist guide

--- assets/script/addFile.js
+++ assets/script/addFile.js
@@ -367,3 +367,27 @@ $(document).ready(function() {
         x--;
     })
 });
+
+
+
+// Tourist guide page auditors
+$(document).ready(function() {
+    var max_fields = 10; //maximum input boxes allowed
+    var wrapper = $(".auditor-input-field"); //Fields wrapper
+    var add_button = $(".add-auditor-field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="row"><div class="col-md-6 my-2"> <label for="" class="form-label">Name</label> <input type="text" class="form-control" /> </div> <div class="col-md-6 my-2"> <label for="" class="form-label"> Address</label> <input type="text" class="form-control" /> </div> <a href="#" class="remove_field">Remove</a> </div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
